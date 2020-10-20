67a2180119f1f144c6f229f0eb43083921db755e
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2020-10-20 22:15:43 +0200

    Implementing ball clearing.

--- src/ball.rs
+++ src/ball.rs
@@ -27,3 +27,40 @@ pub struct Ball {
     pub color : BallColor,
     pub effect : BallEffect,
 }
+
+pub struct BallSequence {
+    pub balls: Vec<Ball>,
+}
+
+pub struct GameState {
+    pub groups: Vec<BallSequence>,
+}
+
+impl BallSequence {
+    fn clear_at(&mut self, index: usize) -> Option<()> {
+        let color_to_clear = self.balls[index].color;
+
+        let balls_to_clear: Vec<usize> = self.balls[..index]
+            .iter()
+            .rev()
+            .take_while(|b| b.color == color_to_clear)
+            .enumerate()
+            .map(|(i, _)| index - i)
+            .chain(
+                self.balls[index..]
+                    .iter()
+                    .take_while(|b| b.color == color_to_clear)
+                    .enumerate()
+                    .map(|(i, _)| index + i),
+            )
+            .collect();
+
+        // If more than 3 balls are contiguous, actually clear them
+        if balls_to_clear.len() >= 3 {
+            self.balls
+                .drain((balls_to_clear.iter().min()?)..(balls_to_clear.iter().max()?));
+        }
+
+        Some(())
+    }
+}
