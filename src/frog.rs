dc08ccc0f9f907a8a34a15ab60eb930689c5bd1b
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2020-10-20 07:36:35 +0200

    Added the different movzmznt options for Frog.

--- src/frog.rs
+++ src/frog.rs
@@ -1,7 +1,13 @@
 use crate::ball::*;
 
+pub enum FrogType {
+    Static(Point),             // If the frog does not move
+    Jumper(Vec<Point>),        // If the frog has multiple possible positions
+    Slider(Point, Point),      // If the frog can move along a slider
+}
+
 pub struct Frog {
-    pub coordinates : Point,
+    pub location : FrogType,
     pub active_ball : Ball,
     pub next_ball : Ball,
 }
